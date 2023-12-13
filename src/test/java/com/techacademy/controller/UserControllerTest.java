f06951e7f503b50eea660ee7cee7e388edcbc8bd
nobil <nobil@192.168.10.29>
2023-12-13 21:50:17 +0900

    Lesson18課題の提出

--- src/test/java/com/techacademy/controller/UserControllerTest.java
+++ src/test/java/com/techacademy/controller/UserControllerTest.java
@@ -7,6 +7,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -37,9 +39,7 @@ class UserControllerTest {
     @BeforeEach
     void beforeEach() {
         // Spring Securityを有効にする
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(webApplicationContext)
-                .apply(springSecurity()).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
     }
 
     @Test
@@ -56,9 +56,35 @@ class UserControllerTest {
 
         // userの検証
         // Modelからuserを取り出す
-        User user = (User)result.getModelAndView().getModel().get("user");
+        User user = (User) result.getModelAndView().getModel().get("user");
         assertEquals(user.getId(), 1);
         assertEquals(user.getName(), "キラメキ太郎");
     }
 
+    @Test
+    @DisplayName("一覧画面")
+    @WithMockUser
+    void testGetList() throws Exception {
+        // HTTPリクエストに対するレスポンスの検証
+        MvcResult result = mockMvc.perform(get("/user/list")) // URLにアクセス
+                .andExpect(status().isOk()) // ステータスを確認
+                .andExpect(model().attributeExists("userlist")) // Modelの内容を確認
+                .andExpect(model().hasNoErrors()) // Modelのエラー有無の確認
+                .andExpect(view().name("user/list")) // viewの確認
+                .andReturn(); // 内容の取得
+
+        // userlistの検証
+        // Modelからuserlistを取り出す
+        @SuppressWarnings("unchecked")
+        List<User> userlist = (List<User>) result.getModelAndView().getModel().get("userlist");
+        // 件数が3件であること
+        assertEquals(userlist.size(), 3);
+        // userlistから1件ずつ取り出し、idとnameを検証する
+        assertEquals(userlist.get(0).getId(), 1);
+        assertEquals(userlist.get(0).getName(), "キラメキ太郎");
+        assertEquals(userlist.get(1).getId(), 2);
+        assertEquals(userlist.get(1).getName(), "キラメキ次郎");
+        assertEquals(userlist.get(2).getId(), 3);
+        assertEquals(userlist.get(2).getName(), "キラメキ花子");
+    }
 }
